7d9751a87b26d2ec6983c7de2d654886f15a205a
liucunhu <786608954@qq.com>
2022-06-28 10:21:18 +0800

    删除多余文件

--- balancedeal.py
+++ balancedeal.py
@@ -9,3 +9,10 @@
 '''
 互联网对账解密及验签
 '''
+class DealBalanceFile:
+    def __init__(self,filename):
+        self.balancefile=filename
+    def get_Aes_key(self):
+        pass
+    def get_file_content(self,):
+        pass
